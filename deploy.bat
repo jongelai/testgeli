28ee74521aecbd4a5d80012de2203e309eb8adeb
jongelai <juangeli@hotmail.com>
2026-02-12 13:41:52 +0100

    Reinicio completo del proyecto

--- deploy.bat
+++ deploy.bat
@@ -7,9 +7,13 @@ echo [2/3] Guardando cambios (Commit)...
 set /p msg="Introduce el mensaje del cambio: "
 git commit -m "%msg%"
 
-echo [3/3] Subiendo a GitHub...
+echo [3/3] Sincronizando y Subiendo...
+:: El comando 'pull --rebase' bajara lo de GitHub y pondra tus archivos encima
+git pull origin main --rebase
+
+echo Enviando a GitHub...
 git push origin main
 
 echo.
-echo ¡Hecho! Tu web se actualizara en 1 minuto.
+echo Intento de envio finalizado. Revisa si hubo errores arriba.
 pause
